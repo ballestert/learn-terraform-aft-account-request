618922168b563ebc4357ca74c6e1939c91c36212
BALLESTER Thomas <TBALLESTER@lucyinthecloud.com>
2023-10-27 16:24:31 +0200

    fit main.tf: changing ManagedOrganizationalUnit to ou-68jw-uvli545u

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,14 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "tballester+20231027aft1@lucyinthecloud.com"
     AccountName               = "aft-provisionned-account-1"
-    ManagedOrganizationalUnit = "Sandbox OU"
+    ManagedOrganizationalUnit = "ou-68jw-uvli545u"
     SSOUserEmail              = "tballester+20231027aft1@lucyinthecloud.com"
     SSOUserFirstName          = "AFT-provisionned user"
     SSOUserLastName           = "1"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Learn Tutorial" = "AFT" 
   }
 
   change_management_parameters = {

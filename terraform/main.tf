5547950856b78f0fa0708dadf053668bf20435e7
BALLESTER Thomas <TBALLESTER@lucyinthecloud.com>
2023-10-30 10:58:23 +0100

    fit main.tf: Add test account for sandbox:ou

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "test_sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tballester+20231030aftsandboxtest@lucyinthecloud.com"
+    AccountName               = "aft-provisionned-account-3"
+    ManagedOrganizationalUnit = "sandbox:ou"
+    SSOUserEmail              = "tballester+20231030aftsandboxtest@lucyinthecloud.com"
+    SSOUserFirstName          = "AFT-provisionned user"
+    SSOUserLastName           = "3"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT" 
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}

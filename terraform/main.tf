3152a60c4786d362edcef0b78b9ace53e24295f7
BALLESTER Thomas <TBALLESTER@lucyinthecloud.com>
2023-10-30 14:37:00 +0100

    fit main.tf: Deploy test lab account for Lab OU with ID (Lab OU (ou-68jw-uf5hq5yo))

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,32 @@ module "test_sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "test_lab" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tballester+20231030afttestlab@lucyinthecloud.com"
+    AccountName               = "aft-provisionned-account-4"
+    ManagedOrganizationalUnit = "Lab OU (ou-68jw-uf5hq5yo)"
+    SSOUserEmail              = "tballester+20231030afttestlab@lucyinthecloud.com"
+    SSOUserFirstName          = "AFT-provisionned lab test"
+    SSOUserLastName           = "4"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT" 
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
